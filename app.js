6bc08168b0559198335dcd9f1f622d1f5d83d0a6
Shamila Pathirana <shamila@vesess.com>
2020-07-20 12:01:29 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+
+//Customize args version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+	command: 'add',
+	describe: 'Add a new note',
+	builder: {
+		title: {
+			describe: 'Note title',
+			demandOption: true,
+			type: 'string'
+		},
+		body: {
+			describe: 'Note body',
+			demandOption: true,
+			type: 'string'
+		}
+	},
+	handler: function(argv){
+		console.log('Title: ' + argv.title)
+		console.log('Body: ' + argv.body)
+	}
+})
+
+// Create remove command
+yargs.command({
+	command: 'remove',
+	describe: 'Remove a note',
+	handler: function(){
+		console.log('Removing the note !')
+	}
+})
+
+// Create list command
+yargs.command({
+	command: 'list',
+	describe: 'List the notes',
+	handler: function(){
+		console.log('Listing all notes')
+	}
+})
+
+// Create read command
+yargs.command({
+	command: 'read',
+	describe: 'Read a note',
+	handler: function(){
+		console.log('Reading a note')
+	}
+})
+
+yargs.parse()
+
+
+
+
---
+++ notes.js
@@ -0,0 +1,7 @@
+// console.log('notes.js')
+
+const getNotes = function(){
+	return 'Your Notes...'
+}
+
+module.exports = getNotes
