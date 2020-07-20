f1221aa5d952c57961b2cf54fe24c0507b941bf6
Shamila Pathirana <shamila@vesess.com>
2020-07-20 16:50:33 +0530

    note add and note remove command added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk')
 const yargs = require('yargs')
+const notes = require('./notes.js')
 
 //Customize args version
 yargs.version('1.1.0')
@@ -21,8 +22,7 @@ yargs.command({
 		}
 	},
 	handler: function(argv){
-		console.log('Title: ' + argv.title)
-		console.log('Body: ' + argv.body)
+		notes.addNotes(argv.title, argv.body)
 	}
 })
 
@@ -30,8 +30,15 @@ yargs.command({
 yargs.command({
 	command: 'remove',
 	describe: 'Remove a note',
-	handler: function(){
-		console.log('Removing the note !')
+	builder: {
+		title: {
+			describe: 'Note title',
+			demandOption: true,
+			type: 'string'
+		}
+	},
+	handler: function(argv){
+		notes.removeNote(argv.title)
 	}
 })
 
--- notes.js
+++ notes.js
@@ -1,7 +1,63 @@
-// console.log('notes.js')
+const fs = require('fs')
+const chalk = require('chalk')
 
 const getNotes = function(){
 	return 'Your Notes...'
 }
 
-module.exports = getNotes
+const addNotes = function(title, body){
+	const notes = loadNotes()
+
+	const duplicateNotes = notes.filter(function(note){
+		return note.title === title
+	})
+
+	if (duplicateNotes.length === 0){
+		notes.push({
+			title: title,
+			body: body
+		})
+
+		saveNotes(notes)
+		console.log('New note added')
+	} else {
+		console.log('Note title taken')
+	}
+
+}
+
+const removeNote = function(title){
+	const notes = loadNotes()
+
+	const notesToKeep = notes.filter(function(note){
+		return note.title !== title
+	})
+	if(notes.length !== notesToKeep.length){
+		saveNotes(notesToKeep)
+		console.log(chalk.bgGreen('Note removed!'))
+	} else {
+		console.log(chalk.bgRed('No note found!'))
+	}
+	
+}
+
+const saveNotes = function(notes){
+	const dataJSON = JSON.stringify(notes)
+	fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = function(){
+	try{
+		const dataBuffer = fs.readFileSync('notes.json')
+		const dataJSON = dataBuffer.toString()
+		return JSON.parse(dataJSON)
+	} catch (e){
+		return []
+	}
+}
+
+module.exports = {
+	getNotes: getNotes,
+	addNotes: addNotes,
+	removeNote: removeNote
+}
